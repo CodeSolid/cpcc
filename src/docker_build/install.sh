7a8df2d9c76f03d6ab373bf17d6037f00ddd0835
John Lockwood <john@codesolid.com>
2024-08-05 09:56:26 -0400

    requirements.txt and install script updates

--- src/docker_build/install.sh
+++ src/docker_build/install.sh
@@ -1,5 +1,5 @@
 !#/usr/bin/bash
 sh -c "$(curl --location https://taskfile.dev/install.sh)" -- -d
 python -m pip install --upgrade pip
-pip install jupyter jupyter_server sympy numpy matplotlib jdesmos jupyterlite-core jupyterlite-pyodide-kernel 
+pip install jupyterlab jupyter_server sympy numpy matplotlib jdesmos jupyterlite-core jupyterlite-pyodide-kernel 
 
